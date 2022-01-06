69e0306303a13b7def3437adc0ecf17ee6d15b60
M.RAY <a.mray47@yahoo.fr>
2022-01-06 19:23:08 +0800

    Modified the Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,59 @@
 <template>
-  <div class="home">
-    Home
+  <div class="splash-container">
+    <div class="splash">
+      <h1>Splendid Food</h1>
+    </div>
   </div>
+
+  <main class="wrapper">
+
+    <h2>Recommended</h2>
+
+    <div class="recommended">
+
+      <div v-for="(product, i) in inventory.slice(0,3)" :key="product.id" class="card">
+        <div class="card-title">
+          {{ product.name }}
+        </div>
+        <div class="card-body">
+          <i class="icofont-10x icofont-{{ product.icon}}"></i>
+          <form>
+            <div class="row">
+              <div class="cell">
+                <label>Type:</label>
+              </div>
+              <div class="cell">
+                <em> {{ product['type'] }}</em>
+              </div>
+            </div>
+            <div class="row">
+              <div class="cell">
+                <label>Price:</label>
+              </div>
+              <div class="cell">
+                ${{ product.price["USD"] }}
+              </div>
+            </div>
+            <div class="row">
+              <div class="cell">
+                <label>Quantity:</label>
+              </div>
+              <div class="cell">
+                <input type="number" v-model.number="product.quantity">
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-light" @click="addToCart(product.name, i)">
+            Add to cart
+          </button>
+        </div>
+      </div>
+
+    </div>
+
+  </main>
 </template>
 
 <script>
