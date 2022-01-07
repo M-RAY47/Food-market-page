88e36b71b3c59a46961c537c3357d952be8f05ad
M.RAY <a.mray47@yahoo.fr>
2022-01-07 21:53:29 +0800

    created compoent SideBar

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Products from '../views/Products.vue';
 import PastOrders from '../views/PastOrders.vue';
+import { SideBar } from '../components/SideBar.vue';
 
 const routes = [
   {
