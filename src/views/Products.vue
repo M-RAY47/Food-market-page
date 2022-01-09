f3cc7d628604283555c192529ba013c04d07cbd0
M.RAY <a.mray47@yahoo.fr>
2022-01-10 07:30:05 +0800

    Add vue for loop instead of hard coded items

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,19 +3,19 @@
     <h1>Products</h1>
 
     <div class="card-container">
-      <div class="card">
+      <div v-for="(product, i) in inventory.slice(0, 6)" :key="product.id" class="card">
         <div class="card-title">
-          Carrots
+          {{ product.name }}
         </div>
         <div class="card-body">
-          <i class="icofont-10x icofont-carrot"></i>
+          <i class="icofont-10x icofont-{{ product.icon }}"></i>
           <form>
             <div class="row">
               <div class="cell">
                 <label>Type:</label>
               </div>
               <div class="cell">
-                <em>Vegetable</em>
+                <em>{{ product.type }}</em>
               </div>
             </div>
             <div class="row">
@@ -23,7 +23,7 @@
                 <label>Price:</label>
               </div>
               <div class="cell">
-                $4.82
+                ${{ product.price }}
               </div>
             </div>
             <div class="row">
@@ -31,13 +31,13 @@
                 <label>Quantity:</label>
               </div>
               <div class="cell">
-                <input type="number" value="0">
+                <input type="number" v-model="product.quantity">
               </div>
             </div>
           </form>
         </div>
         <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
+          <button @click="addToCart(product.name, i)" class="btn btn-light">Add to cart</button>
         </div>
       </div>
 
@@ -238,6 +238,7 @@
 
 export default {
   name: 'Products',
+  props: ['food', 'invetory'],
 };
 
 </script>
