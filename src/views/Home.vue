7b7836ca37346c8958c951d0bef732c4ceea444b
M.RAY <a.mray47@yahoo.fr>
2022-01-13 20:31:35 +0800

    created reusable component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,45 +11,11 @@
 
     <div class="recommended">
 
-      <div v-for="(product, i) in inventory.slice(0,3)" :key="product.id" class="card">
-        <div class="card-title">
-          {{ product.name }}
-        </div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-{{ product.icon}}"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em> {{ product['type'] }}</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">
-                ${{ product.price["USD"] }}
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" v-model.number="product.quantity">
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light" @click="addToCart(product.name, i)">
-            Add to cart
-          </button>
-        </div>
-      </div>
+      <ProductCart
+        v-for="(product, index) in inventory.slice(0,3)"
+        :key="product.id"
+        
+      />
 
     </div>
 
@@ -57,10 +23,13 @@
 </template>
 
 <script>
-
+import ProductCart from '@/components/ProductCart.vue';
 export default {
   name: 'Home',
   props: ['inventory', 'addToCart'],
+  components: {
+    ProductCart
+  },
   data() {
     return {
     };
